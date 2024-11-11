306cd1cdab2aa8063dc7a42df6da27f68dc0145a
AndrewP05 <apuentesm2@ucentral.edu.co>
2024-11-11 00:22:01 -0500

    Add Postgres DB_1
    Implementacion de postgres, aunque solo se realizo en la pagina inicial, tiene la estructura para realizar las demas conexiones :)

---
+++ database_postgresql/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE db_inicio;
+CREATE DATABASE db_admin;
+CREATE DATABASE db_vendedor;
+CREATE DATABASE db_comprador;
--- home/home.py
+++ home/home.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
 CORS(app)
+print("DATABASE URL:", app.config["SQLALCHEMY_DATABASE_URI"])
 
 # Ruta principal
 @app.route('/CULTIVARED')
